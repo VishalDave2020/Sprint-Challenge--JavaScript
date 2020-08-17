848f401b14879c15a8370c2d4b7750ee3c29d505
Vishal <dev@devs-MacBook-Pro.local>
2020-08-16 23:23:10 -0400

    Completed and tested closure.js tasks

--- challenges/closure.js
+++ challenges/closure.js
@@ -5,11 +5,11 @@
 
 const external = "I'm outside the function";
 
-function myFunction() {
+myFunction = () => {
   console.log(external);
   const internal = "Hello! I'm inside myFunction!";
 
-  function nestedFunction() {
+nestedFunction = () => {
     console.log(internal);
   };
   nestedFunction();
@@ -17,8 +17,16 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+/* internal is accessible by nested function bc of scope block the inner nested scope block allow variables closure to those functions.
 
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+const sumation = (param) => {
+  let result = 0;
+  for (let counter = 0; counter <= param; counter++) {
+    result = result + counter;
+  }
+  return result;
+}
+console.log('summation ' + sumation(4));
